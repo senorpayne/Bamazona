50d25c52b0c746f778ffc1124acd17deb754d00e
Jason Payne <senorpayne@yahoo.com>
2018-02-27 16:52:10 -0600

    functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,12 +18,13 @@ var connection = mysql.createConnection({
   connection.connect(function(err,res) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId)
+    console.log("Avaliable to Purchase")
   });
 function inquier1(){
 
     connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
-
+        
 
   inquirer
   .prompt([
@@ -32,18 +33,54 @@ function inquier1(){
       type: "rawlist",
       choices: function() {
         var choiceArray = [];
-        for (var i = 0; i < results.length; i++) {
-          choiceArray.push(results[i].product_name);}
-          return choiceArray;
-      message: "What is the id number of the item you would like to buy?"},
 
-       name:"quanity",
+        for (var i = 0; i < results.length; i++) {
+          var result ={};
+         
+         
+result.id= results[i].id;
+result.name= results[i].product_name;
+result.price = results[i].price;
+
+          choiceArray.push(result); 
+         
+        }
+console.log(choiceArray);
+          
+          return choiceArray;},
+      message: "What is the id number of the item you would like to buy?"
+        },
+        {
+
+       name:"quantity",
       type:"input",
         message: "How many?"
       }
     ])
     .then(function(answer) {
-        // get the information of the chosen item
-        var chosenItem;
-        chosenItem--  })})}
+        
+        var userQuantity = answer.quantity;
+        var productname = answer.product_name;
+        //console.log(productname);
+       connection.query("SELECT * FROM products WHERE product_name ='"+productname+"' ",function(err,result){
+         console.log(result)
+if (userQuantity < result[0].stock){
+console.log("Order Approved");
+
+
+}else {
+console.log("Insufficient quantity!");
+
+}
+       })
+       stockUpdate();
+        })})}
         inquier1();
+
+        function stockUpdate(){
+          var updatedStock = stock - userQuantity;
+          connection.query("UPDATE stock FROM products WHERE stock > 0", function(err, results) {
+            if (err) throw err;
+console.log(results);
+
+        })}
